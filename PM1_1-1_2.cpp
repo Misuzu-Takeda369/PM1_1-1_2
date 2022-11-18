9aa209255fb115a895fab33e03c9e43e8cd3afb7
Misuzu-Takeda369 <115197417+Misuzu-Takeda369@users.noreply.github.com>
2022-11-18 09:39:53 +0900

    リモートリポジトリで変更

--- PM1_1-1_2.cpp
+++ PM1_1-1_2.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello VisualStudio Git!\n";
-    std::cout << "Hello New　changed\n";
+    std::cout << "github clone changed\n
 
     std::cout << "third branch\n";
 
